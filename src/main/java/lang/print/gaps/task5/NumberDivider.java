bdeafb7e9b0bef1143dc9018e03075850b52f12a
Evgenii Demin <evgenii.demin@aboutcontent.de>
2023-02-10 19:09:42 +0100

    Operators

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        int divider = 5;
+        float result = toBeDivided / divider;
+        System.out.println(result);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+        int secondsInMinute = 60;
+        int seconds = 60 * secondsInMinute;
+        System.out.println(seconds);
     }
 }
